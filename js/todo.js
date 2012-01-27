8fa54a8af8e6a6b1c61ef819a7b46d491d0a1b1f
snikliwp <wilk0146@algonquinlive.com>
2012-01-27 10:11:10 -0500

    added ev.preventDefault()

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,7 @@ var data = document.getElementById("item"); // variable containing the informati
 
 var isitempty = function(ev) {
 	// check to see if the text field has something in it
+	ev.preventDefault(); // This stops the form submit from sending the data to the server
 	if (data.value == "") { // if empty let the user know, in no uncertain terms
 		data.className = "textInvalid";
 		alert ("you must put something in the New To-Do Item field")
@@ -45,15 +46,6 @@ var completeClicked = document.getElementById("complete").addEventListener("clic
 	ev.target.parentNode.removeChild(ev.target);
 },false); // end of function completeClicked
 
-document.documentElement.addEventListener("keydown", function(ev) {
-	// look for the return key to enter data
-	switch(ev.keyCode) {
-		case 13 : isitempty();
-		break;
-		default: console.log(ev.keyCode);
-		break;
-		}
-}, false);
-
-// when someone clicks on the submit button move the data in the to-do field to the active box through isitempty
-document.getElementById("submit").addEventListener("click", isitempty, false);
+// when someone clicks on the submit button or selects enter move the data in the to-do field to the active box through isitempty
+// document.getElementById("submit").addEventListener("click", isitempty, false);
+document.getElementById("form").addEventListener("submit", isitempty, false);
